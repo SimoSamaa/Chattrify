867e52839c71ad7b65a92713a5d78cb40a028d8a
SimoSamaa <rayanidbrahim@gmail.com>
2024-11-23 01:46:59 +0100

    set basic router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from "@/components/theme-provider";
+import { RootState } from '@/store/index';
+
+import Home from './pages/HomePage';
+import Login from './pages/LoginPage';
+import Signup from './pages/SignupPage';
+
+import { Button } from '@/components/ui/button';
+
 const App = () => {
+  const data = useSelector((state: RootState) => state.auth.user);
+  console.log('test', data);
+
   return (
-    <>
-      <h1 className="text-3xl">HELLO CHATTRIFTY</h1>
-    </>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Button>Click me</Button>
+    </ThemeProvider>
   );
 };
 
