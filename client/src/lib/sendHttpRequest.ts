f027ec2a2463332e2ad2b095f53a45616268e0bf
SimoSamaa <rayanidbrahim@gmail.com>
2024-12-01 23:51:38 +0100

    add send Http Request function

---
+++ client/src/lib/sendHttpRequest.ts
@@ -0,0 +1,36 @@
+const sendHttpRequest = async <
+  T = Record<string, unknown>,
+  P = object
+>(
+  endPoint: string,
+  method: string = 'GET',
+  payload: P | null = null,
+  page: number | null = 1,
+): Promise<T> => {
+
+  const fetchOptions: RequestInit = {
+    method,
+    // headers: { 'Authorization': `Bearer ${token}` },
+  };
+
+  if (method !== 'GET' && payload !== null) {
+    if (payload instanceof FormData) {
+      fetchOptions.body = payload;
+    } else {
+      fetchOptions.headers = { 'Content-Type': 'application/json' };
+      fetchOptions.body = JSON.stringify(payload);
+    }
+  }
+
+  const req = await fetch(`${import.meta.env.VITE_SERVER_URL}/${endPoint}?page=${page}`, fetchOptions);
+  const res = await req.json();
+
+  if (!req.ok) {
+    const error = new Error(res.message || 'Could not fetch Data!');
+    throw error;
+  }
+
+  return res;
+};
+
+export default sendHttpRequest;
