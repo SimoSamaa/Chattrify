b34dc4bb0c4864259877dbab12bf1fffec62a065
SimoSamaa <rayanidbrahim@gmail.com>
2025-01-16 23:08:18 +0100

    add resize for left and right panel

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,44 @@
-import { Button } from '@/components/ui/button';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '@/store/index';
+import React, { useState, useRef } from 'react';
 import SidebarHeader from '@/components/layouts/SidebarHeader';
 
 const Home = () => {
-  const dispatch: AppDispatch = useDispatch();
+  const [isResize, setIsResize] = useState(false);
+  const leftSide = useRef<HTMLDivElement | null>(null);
+
+  const resize = (e: MouseEvent) => {
+    let newWidth = e.clientX - leftSide.current!.offsetLeft;
+    if (newWidth < 250) newWidth = 250;
+    if (newWidth > window.innerWidth * 0.5) newWidth = window.innerWidth * 0.5;
+    leftSide.current!.style.width = `${newWidth}px`;
+
+  };
+
+  const handelResize = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsResize(true);
+
+    document.body.style.cursor = 'ew-resize';
+    document.addEventListener('mousemove', resize);
+    document.addEventListener('mouseup', () => {
+      setIsResize(false);
+      document.body.style.cursor = 'auto';
+      document.removeEventListener('mousemove', resize);
+    });
+  };
 
   return (
     <div className='pl-[50px]'>
       <SidebarHeader />
-      <h1>chat</h1>
+      <main className='flex h-screen w-full'>
+        <div ref={leftSide} className='overflow-hidden relative w-[30vw] p-4 min-w-[250px] min-w-1/2'>
+
+          <span
+            id='resize'
+            onMouseDown={handelResize}
+            className={`w-[5px] h-full absolute top-0 right-0 select-none cursor-e-resize border-r ${isResize ? 'bg-muted' : 'bg-transparent hover:bg-muted'}`}></span>
+        </div>
+        <div className='flex-1'>messages</div>
+      </main>
     </div>
   );
 };
