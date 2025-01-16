518fcdb165ed7181039725bb2ca5e361894a83ce
SimoSamaa <rayanidbrahim@gmail.com>
2025-01-16 23:38:33 +0100

    save the resize state to localstorage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,16 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SidebarHeader from '@/components/layouts/SidebarHeader';
 
 const Home = () => {
   const [isResize, setIsResize] = useState(false);
   const leftSide = useRef<HTMLDivElement | null>(null);
+  const [width, setWidth] = useState(localStorage['resize']);
+
+  useEffect(() => {
+    if (leftSide.current) {
+      leftSide.current.style.width = `${width}vw`;
+    }
+  }, [width]);
 
   const resize = (e: MouseEvent) => {
-    let newWidth = e.clientX - leftSide.current!.offsetLeft;
-    if (newWidth < 250) newWidth = 250;
-    if (newWidth > window.innerWidth * 0.5) newWidth = window.innerWidth * 0.5;
-    leftSide.current!.style.width = `${newWidth}px`;
+    const newWidthPx = e.clientX - leftSide.current!.offsetLeft;
+    let newWidthPercent = (newWidthPx / window.innerWidth) * 100;
+    if (newWidthPercent < 20) newWidthPercent = 20;
+    if (newWidthPercent > 50) newWidthPercent = 50;
 
+    leftSide.current!.style.width = `${newWidthPercent}vw`;
+
+    setWidth(newWidthPercent);
+    localStorage['resize'] = String(newWidthPercent);
   };
 
   const handelResize = (e: React.MouseEvent) => {
@@ -31,7 +42,6 @@ const Home = () => {
       <SidebarHeader />
       <main className='flex h-screen w-full'>
         <div ref={leftSide} className='overflow-hidden relative w-[30vw] p-4 min-w-[250px] min-w-1/2'>
-
           <span
             id='resize'
             onMouseDown={handelResize}
