43232cb676bf6eb239565f63b2fd8193126a6dca
SimoSamaa <rayanidbrahim@gmail.com>
2025-01-18 19:17:04 +0100

    change padding size in input element

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -70,8 +70,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             onChange={handleInputChange}
             tabIndex={1}
             className={cn(
-              `${type === "search" ? "px-9" : "px-3"
-              } flex h-10 w-full rounded-md border border-input bg-background py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 hover:border-primary focus:border-primary outline-none ring-primary/50 ring-offset-0 focus:ring transition-shadow duration-300 ease-in-out`,
+              `${type === "search" ? "px-9 h-[35px]" : "px-3 h-10"
+              } flex w-full rounded-md border border-input bg-background py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 hover:border-primary focus:border-primary outline-none ring-primary/50 ring-offset-0 focus:ring transition-shadow duration-300 ease-in-out`,
               { "!border-danger !ring-danger/60": error },
               className
             )}
